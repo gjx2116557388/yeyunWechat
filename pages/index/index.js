22a93028897614674c515c8597ee65cac7985595
代码酱 <2116557388@qq.com>
2023-04-11 14:40:17 +0800

    蓝牙连接

--- pages/index/index.js
+++ pages/index/index.js
@@ -53,6 +53,26 @@ Page({
   
   connectBtn(){
     console.log(1)
+    wx.openBluetoothAdapter({
+      success:(res)=>{
+        console.log("初始化蓝牙模块成功",res);
+        //蓝牙适配器初始化成功，开始搜索蓝牙设备
+        wx.startBluetoothDevicesDiscovery({
+          services: [], // 发现所有设备
+          success: function (res) {
+            console.log(res,2333);
+            // 搜索蓝牙设备成功，遍历设备列表连接目标设备
+            for (var i = 0; i < res.devices.length; i++) {
+              var device = res.devices[i];
+              console.log(device,2333);
+            }
+          }
+        })
+      },
+      fail:(res)=>{
+        console.log("初始化失败！！！",res);
+      }
+    })
   }
 
 })
